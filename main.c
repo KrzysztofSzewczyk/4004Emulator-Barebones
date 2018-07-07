57f17306d6b673e00dcb5ea2d36d98f7c1f11fd0
Krzysztof Szewczyk <marekszuwarek1958@gmail.com>
2018-07-07 12:56:34 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,3 @@
+# 4004Emulator-Barebones
+Very simplistic 4004 emulator. All files in this repository are released to Public Domain.
+Creating more advanced features, loading code from images, etc. are left for exercise for You, dear reader.
